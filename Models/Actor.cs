6bd60fed6fab49614f676e8abfa31648e1e23290
benjaminfagioli <benjitafagg@gmail.com>
2026-02-04 09:40:08 -0300

    añadida biografia a actor y cambio en su vista de detalles

--- Models/Actor.cs
+++ Models/Actor.cs
@@ -4,20 +4,13 @@ using System.Collections.Generic;
 namespace StarWars.Models;
 
 public partial class Actor
-{
-  public int IdActor { get; set; }
-
+{  public int IdActor { get; set; }
   public string Nombre { get; set; } = null!;
-
   public string Apellido { get; set; } = null!;
-
+  public string? Biografia {get;set;} = null!;
   public DateOnly FechaNacimiento { get; set; }
-
   public string Nacionalidad { get; set; } = null!;
-
   public string Genero { get; set; } = null!;
-
   public string? Foto { get; set; }
-
   public virtual ICollection<Aparicion> Apariciones { get; set; } = new List<Aparicion>();
 }
--- ViewModels/ActorVM.cs
+++ ViewModels/ActorVM.cs
@@ -1,14 +1,15 @@
 using StarWars.Models;
 namespace StarWars.ViewModels;
+
 public class ActorAparicionVM
 {
   public ActorAparicionVM() { }
 
-  public int IdPelicula {get;set;}
-  public string NombrePelicula {get;set;} = null!;
-  public string NombrePersonaje {get;set;} = null!;
-  public string Descripcion {get;set;} = null!;
-  public string Poster {get;set;} = null!;
+  public int IdPelicula { get; set; }
+  public string NombrePelicula { get; set; } = null!;
+  public string NombrePersonaje { get; set; } = null!;
+  public string Descripcion { get; set; } = null!;
+  public string Poster { get; set; } = null!;
 }
 
 
@@ -48,9 +49,13 @@ public class ActorDetallesVM
   public ActorDetallesVM(Actor a)
   {
     IdActor = a.IdActor;
-    Nombre = a.Nombre;
-    Apellido = a.Apellido;
+    Nombre = $"{a.Nombre} {a.Apellido}";
     Foto = a.Foto;
+    FechaNacimiento = a.FechaNacimiento;
+    Nacionalidad = a.Nacionalidad;
+    Genero = a.Genero;
+    Biografia = a.Biografia;
+
     if (a.Apariciones != null)
       foreach (var aparicion in a.Apariciones)
         Apariciones.Add(new ActorAparicionVM
@@ -64,12 +69,12 @@ public class ActorDetallesVM
   }
 
   public int IdActor { get; set; }
+  public string? Biografia { get; set; }
   public string Nombre { get; set; } = null!;
-  public string Apellido { get; set; } = null!;
   public DateOnly FechaNacimiento { get; set; }
   public string Nacionalidad { get; set; } = null!;
   public string Genero { get; set; } = null!;
   public string? Foto { get; set; }
-  
+
   public virtual ICollection<ActorAparicionVM>? Apariciones { get; set; } = new List<ActorAparicionVM>();
 }
