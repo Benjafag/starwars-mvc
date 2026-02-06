847e9e070b416c4480300d007424cafd7b3501b2
benjaminfagioli <benjitafagg@gmail.com>
2026-02-06 00:45:40 -0300

    mas info de los planetas

--- Controllers/PlanetasController.cs
+++ Controllers/PlanetasController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StarWars;
 using StarWars.Models;
 using StarWars.ViewModels;
@@ -14,13 +15,15 @@ public class PlanetasController : Controller
 
   public IActionResult Index()
   {
-    List<PlanetaIndexVM> res = (from p in _context.Planetas select new PlanetaIndexVM(p)).ToList();
+    List<PlanetaIndexVM> res = (from p in _context.Planetas select new PlanetaIndexVM(p)).ToList().OrderBy(p => p.Region).ToList();
     return View(res);
   }
 
   public IActionResult Detalles(int id)
   {
-    PlanetaDetallesVM? res = new PlanetaDetallesVM(_context.Planetas?.Find(id));
-    return res is null ? RedirectToAction("Index","Home") : View(res);
+    var res = _context.Planetas.Include(p => p.Eventos).Include(p => p.Personajes).Include(p => p.Especies).FirstOrDefault(p=> p.IdPlaneta == id);
+    
+     // inlcuir planetas, personajes
+    return res is null ? RedirectToAction("Index","Home") : View(new PlanetaDetallesVM(res));
   }
 }
--- ViewModels/PlanetaVM.cs
+++ ViewModels/PlanetaVM.cs
@@ -1,5 +1,7 @@
+using StarWars;
 using StarWars.Models;
 
+namespace StarWars.ViewModels;
 public class PlanetaIndexVM
 {
   public PlanetaIndexVM() { }
@@ -21,8 +23,10 @@ public class PlanetaIndexVM
   public string? Foto { get; set; }
 }
 
+
 public class PlanetaDetallesVM
 {
+  private static StarWarsContext _context = new StarWarsContext(); 
   public PlanetaDetallesVM() { }
   public PlanetaDetallesVM(Planeta p)
   {
@@ -37,6 +41,33 @@ public class PlanetaDetallesVM
     Gobierno = p.Gobierno;
     Capital = p.Capital;
     Foto = p.Foto;
+
+    if (p.Eventos is not null)
+      Eventos = p.Eventos.Select(e => new EventoPlanetaVM
+        {
+          IdEvento = e.IdEvento,
+          Titulo = e.Titulo,
+          Fecha = e.Fecha,
+          Imagen = e.Imagen
+        }).ToList();
+
+    if (p.Especies is not null)
+      Especies = p.Especies.Select(e => new EspeciePlanetaVM
+      {
+        IdEspecie = e.IdEspecie,
+        Nombre = e.Nombre,
+      }).ToList();
+
+    if (p.Personajes is not null)
+      foreach (var personaje in p.Personajes)
+      {
+        Personajes.Add(new PersonajePlanetaVM
+        {
+          IdPersonaje = personaje.IdPersonaje,
+          Nombre = personaje.Nombre,
+          Foto = _context.Apariciones.Where(ap => ap.IdPersonaje == personaje.IdPersonaje).OrderBy(ap => ap.Edad).Last().Foto
+        });
+      }
   }
 
   public int IdPlaneta { get; set; }
@@ -50,4 +81,27 @@ public class PlanetaDetallesVM
   public string? Gobierno { get; set; }
   public string? Capital { get; set; }
   public string? Foto { get; set; }
+
+  public List<EventoPlanetaVM> Eventos {get;set;} = new()!;
+  public List<PersonajePlanetaVM> Personajes {get;set;} = new()!;
+  public List<EspeciePlanetaVM> Especies {get;set;} = new();
+}
+
+public class EventoPlanetaVM
+{
+  public int IdEvento { get; set; }
+  public string Titulo { get; set; } = null!;
+  public string? Fecha { get; set; }
+  public string? Imagen { get; set; }
+}
+public class PersonajePlanetaVM
+{
+  public int IdPersonaje { get; set; }
+  public string Nombre { get; set; } = null!;
+  public string Foto { get; set; } = null!;
+}
+public class EspeciePlanetaVM
+{
+  public int IdEspecie { get; set; }
+  public string Nombre { get; set; } = null!;
 }
